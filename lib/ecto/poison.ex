ce0591574188e18b4a20cf95412ee14979bae422
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2017-01-26 13:36:19 +0100

    Try to fix Poison.Encoder.Decimal duplication

--- lib/ecto/poison.ex
+++ lib/ecto/poison.ex
@@ -1,6 +1,8 @@
 if Code.ensure_loaded?(Poison) do
-  defimpl Poison.Encoder, for: Decimal do
-    def encode(decimal, _opts), do: <<?", Decimal.to_string(decimal)::binary, ?">>
+  unless Code.ensure_compiled?(Poison.Encoder.Decimal) do
+    defimpl Poison.Encoder, for: Decimal do
+      def encode(decimal, _opts), do: <<?", Decimal.to_string(decimal)::binary, ?">>
+    end
   end
 
   defimpl Poison.Encoder, for: [EctoOne.Date, EctoOne.Time, EctoOne.DateTime] do
