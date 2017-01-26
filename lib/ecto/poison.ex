0f035fb760d42ec5ed20159ea91281a55ca4b363
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2017-01-26 13:50:19 +0100

    Remove Decimal implementation

--- lib/ecto/poison.ex
+++ lib/ecto/poison.ex
@@ -1,10 +1,4 @@
 if Code.ensure_loaded?(Poison) do
-  unless Code.ensure_compiled?(Poison.Encoder.Decimal) do
-    defimpl Poison.Encoder, for: Decimal do
-      def encode(decimal, _opts), do: <<?", Decimal.to_string(decimal)::binary, ?">>
-    end
-  end
-
   defimpl Poison.Encoder, for: [EctoOne.Date, EctoOne.Time, EctoOne.DateTime] do
     def encode(dt, _opts), do: <<?", @for.to_iso8601(dt)::binary, ?">>
   end
